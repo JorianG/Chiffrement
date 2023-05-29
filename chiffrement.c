801778733bea9a492845513889a3d01784748b39
JorianG <88860300+JorianG@users.noreply.github.com>
2023-05-29 17:02:53 +0000

    Mise en forme des entrées

--- chiffrement.c
+++ chiffrement.c
@@ -33,6 +33,9 @@
 
 
 int is_alphanumeric(char*str){
+  /* Cette fonction detecte les caratères alphanumérique dans une chaine de caractère str
+     renvoie 0 s'il n'y en as pas 
+     renvoie 1 s'il y en as */
     int i;
     for (i = 0; i < strlen(str); i++) {
         if (!isalnum(str[i])) {
@@ -44,6 +47,7 @@ int is_alphanumeric(char*str){
 
 
 const char* dechiffrerCesar(char* texte, int cle) {
+  /* dechiffreCesar dechiffre le texte chiffré (par Cesar) en paramètre par la clé aussi en paramètre*/
     static char *  res ;
     char * msgDechiffrer= (char *) malloc (strlen(texte));
 
@@ -51,10 +55,8 @@ const char* dechiffrerCesar(char* texte, int cle) {
         if ((int) texte[i] >= 97 && (int) texte[i] <= 122) {
             if ((int) texte[i] - cle < 97) {
                 if ((char)97+ ((int) texte[i] - cle-97) >= 0){
-                    // mod negatif
                     msgDechiffrer[i] = (char)97+ ((int) texte[i] - cle-97)+26;
                 }else{
-                    // mod pos
                     msgDechiffrer[i] = (char)97+ ((int) texte[i] - cle-97);
                 }
             } else {
@@ -80,6 +82,7 @@ const char* dechiffrerCesar(char* texte, int cle) {
 
 
 const char* chiffrerCesar(char* texte, int cle) {
+  /* chiffreCesar chiffre le texte en paramètre avec le chiffrement Cesar par la clé en paramètre*/
     static char *  res ;
     char * msgChiffrer= (char *) malloc (strlen(texte));
     for (int i = 0; i <= strlen(texte)-1; i++) {
@@ -97,13 +100,32 @@ const char* chiffrerCesar(char* texte, int cle) {
 
 
 int main() {
-    char* texte;
-    printf("Entrer un texte à chiffrer/déchiffrer");
+    char* texte = (char *) malloc (100);
+    int* cle = (int*) malloc (sizeof(int));
+    printf("---------------------------------------- \n");
+    printf("----------Chiffrement César------------- \n");
+    printf("---------------------------------------- \n");
+  
+    printf("Entrer un texte à chiffrer/déchiffrer :");
     scanf("%s", texte);
+    printf("Entrer une clé (entre 1 et 26) :");
+    scanf("%d", cle);
+    while(*cle <= 0 || *cle >26){
+      printf("Clé invalide, veuillez reessayer ! \n");
+      printf("Entrer une clé (entre 1 et 26) :");
+      scanf("%d", cle);
+    }
+    
     if (is_alphanumeric(texte)) {
-        texte = (char*) chiffrerCesar(texte, 15);
+        
+        texte = (char*) chiffrerCesar(texte, *cle);
+        printf("\n\n\nLe texte chiffré est : ");
         printf("%s\n", texte);
-        printf("%s\n",  dechiffrerCesar(texte, 15));
+
+        printf("par déchiffrement on obtient donc : ");
+        printf("%s\n",  dechiffrerCesar(texte, *cle));
+    } else {
+      printf("Vous avez entré un caractère non alphanumérique invalide.");
     }
     return 0;
 }
